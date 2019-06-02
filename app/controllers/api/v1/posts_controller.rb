4be505b2063a0a071c5d7b857a7564ba61b99fea
sho-kasama <sho-kasama@trunk.fm>
2019-06-03 01:53:19 +0900

    add

---
+++ app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,44 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      
+      
+    def index
+        posts = Post.order(created_at: :desc)
+        render json: { status: 'SUCCESS', message: 'loaded posts', data: posts }
+    end
+    
+
+    def show
+        post = Post.find(params[:id])
+        render json: { status: 'SUCCESS', message: 'loaded the post', data: post }
+    end
+
+    def create 
+        post = Post.new(post_params)
+
+        if post.save
+            render json: { status: 'SUCESS', message: 'loaded the post', data: post }
+        else
+            render json: { status: 'ERROR', message: 'post not saved', data: post.errors}
+        end
+    end
+
+    def update
+        post = Post.find(params[:id])
+        if post.update(post_params)
+          render json: { status: 'SUCCESS', message: 'updated the post', data: post }
+        else
+          render json: { status: 'SUCCESS', message: 'loaded the post', data: post }
+        end
+      end
+
+      private
+
+      def post_params
+        params.require(:post).permit(:title)
+      end
+    end
+  end
+end
+
